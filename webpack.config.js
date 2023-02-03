574a46087e3e1747ec78b5a0fe909a0d6e463a86
Bogdan <bogdan_yalta@mail.ru>
2023-02-03 11:16:07 +0300

    Базовая сборка стилей. MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.[contenthash:8].js",
+        filename: "[name].[contenthash:8].js",
         path: path.resolve(__dirname, "build"),
-        clean: true
+        clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                // use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },n
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+    ],
 };
