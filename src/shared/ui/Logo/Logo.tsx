1267505269e953d1a8b8d21978a41679a82833de
Bogdan <bogdan_yalta@mail.ru>
2023-02-15 10:47:10 +0300

    delete i18n

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { useTranslation } from "react-i18next";
 
 import { LogoProps } from "./Logo.types";
 
@@ -7,14 +6,12 @@ import LogoImg from "shared/assets/img/Logo.png";
 import { Img } from "shared/ui";
 
 export const Logo: FC<LogoProps> = (props) => {
-    const { t } = useTranslation("alt");
-
     const { className, ...otherProps } = props;
 
     return (
         <Img
             src={LogoImg}
-            alt={t("Logo") ?? "Логотип"}
+            alt="Логотип"
             className={className}
             width={80}
             height={60}
